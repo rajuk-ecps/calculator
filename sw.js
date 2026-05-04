2e0bdadc408becb3827c53f15c78784e1c8d649a
rajuk-ecps <75062248+rajuk-ecps@users.noreply.github.com>
2026-05-04 23:03:45 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,13 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker: Installed');
+const CACHE_NAME = 'rajuk-calc-v1'; // প্রতিবার বড় আপডেটে v1 থেকে v2 করবেন
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); // নতুন ভার্সন পাওয়া মাত্রই রেডি হবে
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
 });
