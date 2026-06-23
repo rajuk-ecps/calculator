43e881642daaba548e0e0944a24490e19f4dfb2e
rajuk-ecps <75062248+rajuk-ecps@users.noreply.github.com>
2026-06-23 22:22:26 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'rajuk-calc-v1.0'; // প্রতিবার বড় আপডেটে v1 থেকে v2 করবেন
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); // নতুন ভার্সন পাওয়া মাত্রই রেডি হবে
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
